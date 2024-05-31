15d09f7c46c3968907212d03e68e0f42611a103f
uglynhumble <platon.luk.98@mail.ru>
2024-05-31 17:35:11 +0300

    feat change post

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -30,6 +30,38 @@ post.post('/', async (c) => {
       }
     }
   })
+
+  
   
   return c.json(post)
 }) 
+
+
+post.delete(`/:id`, async (c) => {
+  const postId  = c.req.param('id')
+  const post = await prisma.post.delete({
+    where: {
+      id: Number(postId),
+    },
+  })
+  return c.json(post)
+})
+
+
+post.put('/:id', async (c) => {
+  const updatedPostId = c.req.param('id')
+  const{title, content} = await c.req.json()
+  try {
+    const updatedPost = await prisma.post.update({
+      where:{ id:Number(updatedPostId)},
+      data:{
+        title:title,
+        content:content
+      },
+    });
+    return c.json(updatedPost)
+  }catch(error){
+    return c.json(404)
+  }
+})
+
