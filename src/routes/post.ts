3f9ec096bc50688247230204e108006bcfc3aec9
uglynhumble <platon.luk.98@mail.ru>
2024-05-31 16:40:09 +0300

    feat post add create id post

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,13 +1,35 @@
 import { Hono } from "hono";
+import { prisma } from "..";
 
 export const post = new Hono()
 
 
 post.get('/', (c) => c.text('List Posts')) 
 
-post.get('/:id', (c) => {
+post.get('/:id', async (c) => {
   const id = c.req.param('id')
-  return c.text('Get Post: ' + id)
+  const getPost = await prisma.post.findUnique({
+    where:{
+      id:Number(id)
+    }
+  }) 
+
+  return c.json(getPost)
 
 })
-post.post('/', (c) => c.text('Create Post')) 
+post.post('/', async (c) => {
+  const{userId, title, content} = await c.req.json();
+  const post = await prisma.post.create({
+    data: {
+      title,
+      content,
+      user: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  })
+  
+  return c.json(post)
+}) 
