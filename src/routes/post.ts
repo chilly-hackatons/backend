15f77d3f70ac6849b88287fcfbc2bfddd5b1861f
uglynhumble <platon.luk.98@mail.ru>
2024-06-01 15:37:22 +0300

    feat:changes

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,44 +1,49 @@
-import { Hono } from "hono";
-import { prisma } from "..";
+import { Hono } from 'hono'
+import { prisma } from '..'
 
 export const post = new Hono()
 
+post.get('/comments', async (c) => {
+  const retPostWithComms = await prisma.post.findMany({
+    include: {
+      comments: true,
+    },
+  })
+  return c.json(retPostWithComms)
+})
 
 //return post
 post.get('/:id', async (c) => {
-  const id = c.req.param('id')
+  const postId = c.req.param('id')
   const getPost = await prisma.post.findUnique({
-    where:{
-      id:Number(id)
-    }
-    
-  }) 
+    where: {
+      id: Number(postId),
+    },
+  })
 
   return c.json(getPost)
-//add post
 })
+//add post
 post.post('/', async (c) => {
-  const{userId, title, content} = await c.req.json();
+  const { userId, title, content } = await c.req.json()
   const post = await prisma.post.create({
     data: {
       title,
       content,
       user: {
         connect: {
-          id: userId
-        }
-      }
-    }
+          id: userId,
+        },
+      },
+    },
   })
 
-  
-  
   return c.json(post)
-}) 
+})
 
 //delete post
 post.delete(`/:id`, async (c) => {
-  const postId  = c.req.param('id')
+  const postId = c.req.param('id')
   const post = await prisma.post.delete({
     where: {
       id: Number(postId),
@@ -50,33 +55,23 @@ post.delete(`/:id`, async (c) => {
 //change post
 post.put('/:id', async (c) => {
   const updatedPostId = c.req.param('id')
-  const{title, content} = await c.req.json()
+  const { title, content } = await c.req.json()
   try {
     const updatedPost = await prisma.post.update({
-      where:{ id:Number(updatedPostId)},
-      data:{
-        title:title,
-        content:content
+      where: { id: Number(updatedPostId) },
+      data: {
+        title: title,
+        content: content,
       },
-    });
+    })
     return c.json(updatedPost)
-  }catch(error){
+  } catch (error) {
     return c.json(404)
   }
 })
 //return all posts
-post.get('/', async (c)=> {
+post.get('/', async (c) => {
   const getAllPosts = await prisma.post.findMany()
 
   return c.json(getAllPosts)
-})
-
-post.get('/:id/comments', async (c)=>{
-    const postCommsId = c.req.param('id')
-    const retPostWithComms = await prisma.comment.findMany({
-      where:{
-        id:Number(postCommsId)
-      },
-    })
-    return c.json(retPostWithComms)
 })
