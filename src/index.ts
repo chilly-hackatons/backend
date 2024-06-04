f1843fd83a6e92882c8039dec5e22b8de3ee901a
zeroqs <smthq@bk.ru>
2024-06-04 03:09:55 +0300

    chore: try vercel

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,17 @@ import { vacancy } from './routes/vacancy'
 import { cors } from 'hono/cors'
 import { profile } from './routes/profile'
 import { serve } from '@hono/node-server'
+import { handle } from '@hono/node-server/vercel'
 
 export const prisma = new PrismaClient()
 
-const app = new Hono()
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const app = new Hono().basePath('/api')
 
 app.use(
   cors({
@@ -75,10 +82,11 @@ app.route('/auth', auth)
 
 app.route('/profile', profile)
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+// const port = 3000
+// console.log(`Server is running on port ${port}`)
 
-serve({
-  fetch: app.fetch,
-  port,
-})
+// serve({
+//   fetch: app.fetch,
+//   port,
+// })
+export default handle(app)
